6112538533bcfd52931d853e36aca45c5e3a86e3
Éloi Strée <eloistree@gmail.com>
2025-03-09 01:12:29 +0100

    Save Input of steam deck from a mapping using not language key

---
+++ SteamDeckNodeInput.cs
@@ -0,0 +1,16 @@
+///--------------------------------------------///
+///-----MADE WITH: UNODE VISUAL SCRIPTING-----///
+///------------------------------------------///
+#pragma warning disable
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace be.elab.steamdeckinput {
+	/// <summary>
+	/// Input Steam Deck Graph
+	/// </summary>
+	public class SteamDeckNodeInput {}
+	public class newClass {}
+
+}
